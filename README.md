ff859d3338cc6c1093f23089fe4c06fcc78b9545
LeticiadelaOsa <deletidev@gmail.com>
2023-11-22 13:47:54 +0100

    cambio plantilla vite

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# typescript-react-sandbox
+# VITE TEMPLATE
+
+Template de vite, preparado para React, Typescript, testing con Vitest, Emotion para estilado css, plugin para minificar imagenes, usando variables de entorno, alias para rutas, y configuración de salida separando vendor y organizando tipos de archivos en carpetas.
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
@@ -9,9 +10,48 @@ const vitestConfig: VitestUserConfigInterface = {
     globals: true
   }
 };
-
 export default defineConfig({
-  plugins: [checker({ typescript: true }), react()],
+  base: '',
+  root: './src',
+  plugins: [
+    checker({ typescript: true }),
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    }),
+    ViteImageOptimizer({})
+  ],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: entryInfo => {
+          return 'assets/js/[name]-[hash].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name?.match(/\.css$/)) {
+            return 'assets/css/[name]-[hash][extname]';
+          }
+          if (assetInfo.name?.match(/\.(png|jpg|gif|svg)$/)) {
+            return 'assets/images/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+        manualChunks: {
+          react: ['react', 'react-dom']
+        },
+        chunkFileNames: chunckInfo => {
+          if (chunckInfo.name?.match(/^react/)) {
+            return 'assets/js/[name]-[hash].js';
+          }
+          return '[name]-[hash].js';
+        }
+      }
+    }
+  },
   test: vitestConfig.test,
   resolve: {
     alias: {
